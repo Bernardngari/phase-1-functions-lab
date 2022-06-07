0ce677bce6809adad18ebf8adaac289bd7b40368
Bernardngari <bernardngari.ke@gmail.com>
2022-06-07 18:25:30 +0300

    commit tree da7d59d2299cb7fc27a5f3eaeb94075732e3a220

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (distance) =>{
+  if(distance > 42){
+    return distance - 42;
+  }
+  else{
+    return 42 - distance;
+  }
+}
+
+const distanceFromHqInFeet = (distance) =>{
+  let feet = distanceFromHqInBlocks( distance );
+  return feet * 264;
+}
+
+const distanceTravelledInFeet = (loci, destination) => {
+  if ( destination > loci){
+   return  ( destination - loci ) * 264;
+  }
+  else{
+    return ( loci - destination ) * 264;
+  }
+}
+
+const calculatesFarePrice = (loci, destination) => {
+   const distance = distanceTravelledInFeet(loci, destination);
+   if(distance <= 400) {
+      return 0;
+   } else if(distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+   } else if(distance > 2000 && distance <= 2500) {
+      return 25;
+   } else {
+      return 'cannot travel that far';
+   }
+}
